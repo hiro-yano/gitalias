6e0eb2dac6dddbdbb7a1f1d7565b07be112c5b2e
yappynoppy <hyano1991@gmail.com>
2019-10-26 13:25:38 +0900

    remove collect log

--- collect_log.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ssh vagrant@$1 "sudo tar -cvf - -C /usr/local/src/ apache-tomcat-9.0.27/logs/ -C /var/opt/test/ abc/logs -C /var/lib/pgsql/11/logs/" | gzip >> logs.tar.gz
-#ssh vagrant@vm-cluster-node2 "sudo tar -cf - -C /var/opt/test/ abc/logs" | gzip >> logs.tar.gz
